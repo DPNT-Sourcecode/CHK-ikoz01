2fa3620ac9feb5f6eb45f2de1af7c5bc83b3edee
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 11:18:04 +0000

    Wed Apr 02 11:18:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,32 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-# Our price table and offers:
-# +------+-------+----------------+
-# | Item | Price | Special offers |
-# +------+-------+----------------+
-# | A    | 50    | 3A for 130     |
-# | B    | 30    | 2B for 45      |
-# | C    | 20    |                |
-# | D    | 15    |                |
-# +------+-------+----------------+
-
-
-# Notes:
-#  - For any illegal input return -1
-
-# In order to complete the round you need to implement the following method:
-
-# checkout(string) -> integer
-#  - param[0] = a string containing the SKUs of all the products in the basket
-#  - @return = an integer representing the total checkout value of the items
-
-from collections import int
+from collections import defaultdict
+value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15}
+special_price = {'A': {"quantity": 3, 'price': 130}, 'B': {"quantity": 2, 'price': 45}}
 
 def checkout(skus):
-    value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15}
-    special_price = {'A': {"quantity": 3, 'price': 130}, 'B': {"quantity": 2, 'price': 45}}
+    """
+    Returns the checkout value of all skus.
 
+    Args :
+    skus : string containing all items
+
+    Returns: 
+    An integer with sum of price of all skus including offers.
+
+    -1 if sku is not present in value_price_map
+    """
     sku_quantity_map = defaultdict(int)
     for char in skus:
         sku_quantity_map[char] +=1
@@ -49,8 +39,8 @@ def checkout(skus):
 
         total_price += min_price
 
+    return total_price
 
-print(checkout("AAB"))
 
 
 
