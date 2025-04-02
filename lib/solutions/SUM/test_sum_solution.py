6e47b629d09514ac943c305f568f9412d187d353
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 10:27:10 +0000

    Wed Apr 02 10:27:10 UTC 2025

--- lib/solutions/SUM/test_sum_solution.py
+++ lib/solutions/SUM/test_sum_solution.py
@@ -1,7 +1,14 @@
 import pytest
+import sys
+from pathlib import Path
+
+# Add the project root to sys.path
+sys.path.append(str(Path(__file__).parent.parent)) 
 from SUM.sum_solution import compute
 
 
-def test_compute_with_positive_int_within_range() -> None:
+def test_compute_with_valid_input() -> None:
 
     assert compute(1,2) == 3
+    assert compute(0, 100) == 100
+    assert compute(100, 99) == 199
