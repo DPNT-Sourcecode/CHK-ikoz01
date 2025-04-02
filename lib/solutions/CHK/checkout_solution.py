f879b2066dc5b4613380e4a4c945d375a9f7cc2c
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 11:03:04 +0000

    Wed Apr 02 11:03:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,30 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+# Our price table and offers:
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
+
+# Notes:
+#  - For any illegal input return -1
+
+# In order to complete the round you need to implement the following method:
+
+# checkout(string) -> integer
+#  - param[0] = a string containing the SKUs of all the products in the basket
+#  - @return = an integer representing the total checkout value of the items
+
+
+
 def checkout(skus):
+    value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15}
     raise NotImplementedError()
+
