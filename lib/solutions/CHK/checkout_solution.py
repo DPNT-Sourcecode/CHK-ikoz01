99f6c7d3dd75ef906049cff7e26998ce99ec5fba
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 13:01:21 +0000

    Wed Apr 02 13:01:21 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,14 +1,9 @@
-
-
-# noinspection PyUnusedLocal
-# skus = unicode string
-
 from collections import defaultdict
 
 value_price_map = {
     'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60,
     'K': 70, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50, 'S': 20, 'T': 20,
-    'U': 40, 'V': 50, 'W': 20, 'X': 17, 'Y': 20, 'Z': 21
+    'U': 40, 'V': 50, 'W': 20, 'X': 17, 'Y': 20, 'Z': 20
 }
 
 special_price = {
@@ -133,5 +128,6 @@ def checkout(skus):
     total_price += group_offer_price
     return total_price
 
+print(checkout("STXZ"))
 
 
