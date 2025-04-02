ac9ac3e27e17e537e24aa409c8f2923090c06671
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 12:22:26 +0000

    Wed Apr 02 12:22:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,29 @@
 # skus = unicode string
 
 from collections import defaultdict
-value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15, 'E':40, 'F':10}
-special_price = {'A': [{"quantity": 5, 'price': 200},{"quantity": 3, 'price': 130}, ], 'B': [{"quantity": 2, 'price': 45}], 
-'E': [{"quantity": 2, 'free': {'B':1}}], 'F': [{"quantity": 2, 'free': {'F':1}}]}
+# value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15, 'E':40, 'F':10}
+# special_price = {'A': [{"quantity": 5, 'price': 200},{"quantity": 3, 'price': 130}, ], 'B': [{"quantity": 2, 'price': 45}], 
+# 'E': [{"quantity": 2, 'free': {'B':1}}], 'F': [{"quantity": 2, 'free': {'F':1}}]}
+value_price_map = {
+    'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60,
+    'K': 80, 'L': 90, 'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50, 'S': 30, 'T': 20,
+    'U': 40, 'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50
+}
+
+special_price = {
+    'A': [{"quantity": 5, 'price': 200}, {"quantity": 3, 'price': 130}],
+    'B': [{"quantity": 2, 'price': 45}],
+    'E': [{"quantity": 2, 'free': {'B': 1}}],
+    'F': [{"quantity": 2, 'free': {'F': 1}}],
+    'H': [{"quantity": 10, 'price': 80}, {"quantity": 5, 'price': 45}],
+    'K': [{"quantity": 2, 'price': 150}],
+    'Q': [{"quantity": 3, 'price': 80}],
+    'R': [{"quantity": 3, 'free': {'Q': 1}}],
+    'U': [{"quantity": 3, 'free': {'U': 1}}],
+    'V': [{"quantity": 3, 'price': 130}, {"quantity": 2, 'price': 90}],
+    'P': [{"quantity": 5, 'price': 200}],
+    'N': [{"quantity": 3, 'free': {'M': 1}}]
+}
 
 def checkout(skus):
     """
@@ -27,6 +47,17 @@ def checkout(skus):
     free_B = 0
     total_price = 0
     if sku_quantity_map["E"] > 0:
+        offer = special_price["E"][0]
+        offer_quantity = offer.get('quantity')
+        offer_free_item = offer.get('free')['B']
+        offer_free_item_sku = 'B'
+
+        quantity_E = sku_quantity_map["E"] 
+        free_B = offer_free_item * (quantity_E // offer_quantity)
+        if sku_quantity_map["B"] - free_B >= 0:
+            sku_quantity_map["B"] -= free_B
+
+    if sku_quantity_map["N"] > 0:
         offer = special_price["E"][0]
         offer_quantity = offer.get('quantity')
         offer_free_item = offer.get('free')['B']
@@ -78,5 +109,6 @@ def checkout(skus):
 
 
 print(checkout("FFFF"))
+
 
 
