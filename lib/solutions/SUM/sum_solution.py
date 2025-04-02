cd3f7d0a5dcfd1a42b528c4bb155259a0767b0e6
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 10:06:10 +0000

    Wed Apr 02 10:06:10 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,23 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x: int, y: int): -> int
+def compute(x: int, y: int) -> int
+    """
+    Returns the result of summation of two numbers between 0 and 100
+
+    Args :
+    x : integer between 0 to 100
+    y : integer between 0 to 100
+
+    Returns: 
+    An integer which is summation of x and y
+
+    Raise ValueError if arguments are outside the range [0,100]
+    """
+    if x >= 0 and x <= 100:
+        raise ValueError("x must be between 0 and 100")
+
+    if y >= 0 and y <= 100:
+        raise ValueError("y must be between 0 and 100")
+
     return x + y
 
 
---
+++ lib/solutions/SUM/test_sum_solution.py
@@ -0,0 +1,3 @@
+def test_sum_solution_args_within_range() -> None:
+
+    assert(compute(1,2), 3)
