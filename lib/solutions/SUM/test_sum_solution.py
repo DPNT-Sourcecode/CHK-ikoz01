f054e50908ba4fef5203b9e678552b98407c1fe1
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 10:12:10 +0000

    Wed Apr 02 10:12:10 UTC 2025

--- lib/solutions/SUM/test_sum_solution.py
+++
@@ -1,7 +0,0 @@
-def test_sum_solution_args_within_range() -> None:
-
-    assert compute(1,2) == 3
-
-# def test_sum_solution_args_args_outside_range() -> ValueError:
-
-#     assert compute(-1,2) == 3
