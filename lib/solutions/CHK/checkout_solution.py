c1d7a3ece1b4885f756840e35ea2829ef70957aa
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 11:06:04 +0000

    Wed Apr 02 11:06:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,9 +23,15 @@
 #  - param[0] = a string containing the SKUs of all the products in the basket
 #  - @return = an integer representing the total checkout value of the items
 
-
+from collections import int
 
 def checkout(skus):
     value_price_map = {'A':50, 'B': 30, 'C':20, 'D':15}
+    special_price = {'A': [{"quantity": 3, 'price': 130}], 'B': [{"quantity": 2, 'price': 45}]}
+
+    sku_quantity_map = defaultdict(int)
+    for char in skus:
+        sku_quantity_map[char] +=1
     raise NotImplementedError()
+
 
