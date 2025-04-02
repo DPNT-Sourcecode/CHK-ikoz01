bf843ca9518c53eeba50cb0890a4283bca1b96bb
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 10:39:10 +0000

    Wed Apr 02 10:39:10 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -15,7 +15,7 @@ def compute(x: int, y: int) -> int:
     if x < 0 or x > 100:
         raise ValueError("x must be between 0 and 100")
 
-    if y < 0 and y > 100:
+    if y < 0 or y > 100:
         raise ValueError("y must be between 0 and 100")
 
     return x + y
