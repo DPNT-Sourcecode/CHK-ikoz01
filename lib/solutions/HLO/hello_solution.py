2ccb5dd9f2da95f98358415e30629e47fee765d3
sbx_user1051 <sbx_user1051@169.254.68.55>
2025-04-02 10:45:26 +0000

    Wed Apr 02 10:45:26 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,15 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name: str) -> str:
+    """
+    Returns the string with Hello <friend_name>
 
+    Args :
+    friend_name : string
+
+    Returns: 
+    A string containing "Hello <friend_name>"
+    """
     return "Hello {}", friend_name
+
 
